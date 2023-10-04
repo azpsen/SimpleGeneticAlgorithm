ffbdbacb5aa6672bc087e9ed8f9506ae9923c783
april <aprl.petersen@gmail.com>
2023-10-04 08:54:20 -0500

    Add function stubs

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import robby
 
+
+def generate_genome():
+    pass
+
+
+def mutate_genome(genome):
+    pass
+
+
+def crossover_genomes(g1, g2):
+    pass
+
+
+def fitness(genome):
+    pass
+
+
+def sort_by_fitness(genomes):
+    tuples = [(fitness(g), g) for g in genomes]
+    tuples.sort()
+    sorted_fitness_values = [f for (f, g) in tuples]
+    sorted_genomes = [g for (f, g) in tuples]
+    return sorted_genomes, sorted_fitness_values
+
+
 rw = robby.World(10, 10)
 
 rw.demo(rw.strategyM)
